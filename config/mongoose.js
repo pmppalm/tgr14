b53b9c61b37b92a9b24f960e878309ab78592c98
Phatin Assawaratanawarang <43286845+pmppalm@users.noreply.github.com>
2019-10-10 16:33:17 +0700

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -3,10 +3,10 @@ var mongoose = require('mongoose');
 
 module.exports = function () {
     mongoose.set('debug', config.debug);
-    //var db = mongoose.connect(config.mongoUri);
+    var db = mongoose.connect(config.mongoUri);
     //var db = mongoose.connect('mongodb://tgr13:silpakorn13@ds235775.mlab.com:35775/heroku_jm7p5fsc');
     //var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs');
-    var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs');
+    //var db = mongoose.connect('mongodb://heroku_jvp8kncs:tloup834159i629f624onibgbn@ds117101.mlab.com:17101/heroku_jvp8kncs');
     require('../app/models/user.model');
     require('../app/models/exam.model');
     return db;
